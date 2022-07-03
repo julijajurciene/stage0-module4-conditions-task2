88799ab17432fd34b96669815819927670fc77ee
Julija <julija.jurciene@gmail.com>
2022-07-03 11:10:04 +0300

    commit

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
-
+        String result;
+        if (from1UpTo1000 < 500) {
+            result = "Eagle";
+        } else {
+            result = "Tail";
+        }
+        System.out.println(result);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        int result;
+        if (first >= second) {
+            result = first;
+        } else {
+            result = second;
+        }
+        System.out.println(result);
     }
 }
